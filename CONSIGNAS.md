fe17a38ce8df7dd47aee424f6456f7d41380bf3f
martinezmatias0902 <martinez.matias0902@gmail.com>
2020-07-19 15:39:52 -0300

    Alkemy Challenge

---
+++ CONSIGNAS.md
@@ -0,0 +1,13 @@
+# Ejercicio
+
+Completar el archivo main.js (ubicado en la raíz del proyecto) con el código. 
+
+## 1
+Cuando el sitio haya cargado, deberás mostrar un prompt() que solicite el nombre de usuario. Una vez completado, modificar el mensaje "Bienvenido Usuario" que aparece en la sección con id "banner" por "Bienvenido " seguido del nombre de usuario ingresado.
+
+## 2
+Dentro de la sección con id "intro" hay 2 etiquetas <section> con la clase "first" y "second", siendo una un elemento destacada y la otra no.
+Deberás hacer que, al realizar click sobre alguna de ellas, se alternen de forma individual, es decir, si la apariencia del section era la de "Elemento Destacado" deberá tener la de "Elemento secundario", y viceversa, modificando el color del ícono y el texto que corresponda.
+
+## 3
+Cuando el usuario haga click en el botón dentro de la sección con id "main", se deberá agregar un nuevo div en el listado inferior, con la clase "list-element". Dentro del mismo se deberá respetar el orden existente, es decir, el siguiente deberá contener como título Item 3, el siguiente Item 4, y así consecutivamente.
---
+++ main.js
@@ -0,0 +1,87 @@
+// Escribir el código solamente en este archivo
+document.addEventListener("DOMContentLoaded", async() => {
+    
+    //DOM Variables
+    const banner = document.querySelector("#banner header h2");
+    const introSection = document.querySelector("#intro");
+    const firstSection = document.querySelector(".first i");
+    const secondSection = document.querySelector(".second i");
+    let firstSectionText = document.querySelector('.first header h2');
+    let secondSectionText = document.querySelector('.second header h2')
+    const itemsButton = document.querySelector("#itemsButton");
+    const itemsSection = document.querySelector("#itemsSection");
+
+    //Username input
+    const welcome = username => {
+        username = prompt("Ingrese nombre de usuario: ");
+        banner.innerHTML = "Bienvenido " + username; 
+    }
+    welcome();
+
+    //Swapping first and second elements
+    const mainElement = element => {
+        // Text to show
+        const textoDestacado = "Elemento destacado";
+        const textoSecundario = "Elemento secundario"
+
+        //Main classes
+        mainClass = document.querySelector('.first i').getAttribute('class');
+        secondClass = document.querySelector('.second i').getAttribute('class');
+        
+        
+        //Toggle main texts and classes
+        introSection.addEventListener('click', () => {
+            
+            if ( document.querySelector('.first i').className.match(mainClass)) {
+                firstSectionText.innerHTML = textoSecundario;
+                secondSectionText.innerHTML = textoDestacado;
+                firstSection.setAttribute("class", secondClass);
+                secondSection.setAttribute("class", mainClass);
+
+            } else {
+                firstSectionText.innerHTML = textoDestacado;
+                secondSectionText.innerHTML = textoSecundario;
+                secondSection.setAttribute("class", secondClass);
+                firstSection.setAttribute("class", mainClass);
+            }
+        })
+    }
+    mainElement()
+
+    //Incorporate items to "My works" section 
+    let contador = 2;
+    itemsButton.addEventListener('click', () => {
+        //Create HTML needed elements
+        let div = document.createElement("div");
+        let section = document.createElement("section");
+        let header = document.createElement("header");
+        let h3 = document.createElement("h3");
+        let p = document.querySelector('#secondDiv section p').cloneNode(true);
+
+        //Div and section classes clonning
+        divClass = document.querySelector('#secondDiv').getAttribute('class');
+        sectionClass = document.querySelector('.box').getAttribute('class');
+        div.setAttribute("class", divClass);
+        section.setAttribute('class', sectionClass);
+
+        //Item + number
+        contador ++;
+        h3.innerHTML = "Item " + contador;
+
+        //DOM manipulation
+        itemsSection.appendChild(div)
+        div.appendChild(section)
+        section.appendChild(header)
+        header.appendChild(h3)
+        section.appendChild(p)
+    });
+
+
+
+   /*  itemsButton.addEventListener('click', () => {
+        
+        const div = document.getElementById('secondDiv');
+        let newDiv = div.cloneNode(true);
+        itemsSection.appendChild(newDiv); */
+    
+});
